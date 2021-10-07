6b610676c79eb9edb6a7f53e01fa203e4d3057de
muhanggaohs <angga.oktaharisetia@gmail.com>
2021-10-07 14:19:35 +0700

    Elements Button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,20 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
 export default function Button(props) {
-    return <div></div>;
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hashShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.isDisabled || props.isLoading) {
+    if (props.isDisabled) className.push("disabled");
+    return (
+      <span className={className.join(" ")} style={props.style}>
+        {props.isLoading ? (
+          <>
+            <span className="spinner-border.spinner-border-sm.mx-5"></span>
+            <span className="sr-only">Loading....</span>
+          </>
+        ) : (
+          props.children
+        )}
+      </span>
+    );
+  }
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener nereferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
+  return (
+    <button
+      to={props.href}
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 Button.prototype = {
-    type: propTypes.oneOf(["button", "link"]),
-    onclick: prototype.func,
-    target: prototype.string,
-    className: prototype.string,
-    isDisables: prototype.bool,
-    isLoading: prototype.bool,
-    isSmall: prototype.bool,
-    isLarge: prototype.bool,
-    isBlock: prototype.bool,
-    hashShadow: prototype.bool,
-}
+  type: propTypes.oneOf(["button", "link"]),
+  onClick: propTypes.func,
+  href: propTypes.string,
+  target: propTypes.string,
+  className: propTypes.string,
+  isExternal: propTypes.bool,
+  isLoading: propTypes.bool,
+  isDisabled: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  hashShadow: propTypes.bool,
+};
